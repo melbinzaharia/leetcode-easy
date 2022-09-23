6ffdba6ca9a17fb0d25c673c26d44f4ae1206ad0
Melbin Zacharia <102401711+melbinzaharia@users.noreply.github.com>
2022-09-24 02:45:45 +0530

    Create groupOfFriends.java

---
+++ groupOfFriends.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            String s=in.next();
+            String[] arr=s.split("0",0);
+            // System.out.println(Arrays.toString(arr));
+             print(arr);
+        }
+        
+    }
+    public static void print(String[] arr){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(!arr[i].equals("")){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
