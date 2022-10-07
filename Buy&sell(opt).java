8cd3835091e7eeb54f96f1371d5fc857c34bb14f
Melbin Zacharia <102401711+melbinzaharia@users.noreply.github.com>
2022-10-07 12:18:38 +0530

    Create Buy&sell(opt).java

---
+++ Buy&sell(opt).java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int diff = 0;
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        
+for(int i=0;i<prices.length;i++){
+    if(prices[i]<min){
+        min= prices[i];
+    }else if(prices[i]-min>max){
+        max=prices[i]-min;
+    }
+}
+       return max;
+                             
+    
+    }
+    }
